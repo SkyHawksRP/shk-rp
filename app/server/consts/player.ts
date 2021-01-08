c574aa2f9202517f35b8257a8b1be693d6ac9f8d
Breno Pereira <Brenopereira856@gmail.com>
2021-01-07 22:50:47 -0300

    adiciona consts de player para level e adminlvl

---
+++ app/server/consts/player.ts
@@ -0,0 +1,12 @@
+// Admin Level
+const USER_DEV = 3;
+const USER_ADMIN = 2;
+const USER_MOD = 1;
+
+export { USER_DEV, USER_ADMIN, USER_MOD };
+
+// Player level
+const LEVEL_COST = 2000; // per level
+const EXPERIENCE_REQUIRED = 3 // per level
+
+export { LEVEL_COST, EXPERIENCE_REQUIRED };
