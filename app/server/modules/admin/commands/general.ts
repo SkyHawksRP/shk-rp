95cf7b8da554391b93dedcb3a1ea506382de5633
Breno Pereira <Brenopereira856@gmail.com>
2021-01-14 21:45:17 -0300

    adiciona comando de criar veiculo

--- app/server/modules/admin/commands/general.ts
+++ app/server/modules/admin/commands/general.ts
@@ -1,18 +1,19 @@
 import { COLOR } from '../../../consts/colors';
 import { findPlayer } from '../../../utils/misc';
+import { USER_DEV, USER_ADMIN, USER_MOD } from '../../../consts/player';
 
 const kickCommand = (player: PlayerMp, fullText: string, target: string, ...args: Array<string>) => {
     if(!fullText || target) return player.outputChatBox(`${COLOR.INFO}* /kick [playerid] [motivo]`);
 
     const newTarget = findPlayer(target);
-    if (player.adminLevel < 1) player.outputChatBox(`!${COLOR.ERROR}* Você não tem permissão.`);
-    else if (!newTarget) player.outputChatBox(`!${COLOR.ERROR}* Jogador não encontrado.`);
-    else if (Array.isArray(newTarget)) player.outputChatBox(`!${COLOR.ERROR}* Múltiplos jogadores encontrados.`);
+    if(player.adminLevel < USER_MOD) player.outputChatBox(`!${COLOR.ERROR}* Você não tem permissão.`);
+    else if(!newTarget) player.outputChatBox(`!${COLOR.ERROR}* Jogador não encontrado.`);
+    else if(Array.isArray(newTarget)) player.outputChatBox(`!${COLOR.ERROR}* Múltiplos jogadores encontrados.`);
     else {
         let reason = '';
-        if (newTarget != player) {
+        if(newTarget != player) {
         mp.players.sendChatMessage(`!${COLOR.ADMIN_ACTION}* ${player.name} kickou ${newTarget.name} do servidor.`, 1, false); // global
-            if (args.length > 0) {
+            if(args.length > 0) {
                 reason = args.join(' ');
                 mp.players.sendChatMessage(`!${COLOR.ADMIN_ACTION}* Motivo: ${reason}.`, 1, false); // global
             }
@@ -26,14 +27,14 @@ const banCommand = (player: PlayerMp, fullText: string, target: string, ...args:
     if(!fullText || target) return player.outputChatBox(`${COLOR.INFO}* /ban [playerid] [motivo]`);
 
     const newTarget = findPlayer(target);
-    if (player.adminLevel < 1) player.outputChatBox(`!${COLOR.ERROR}* Você não tem permissão.`);
-    else if (!newTarget) player.outputChatBox(`!${COLOR.ERROR}* Jogador não encontrado.`);
-    else if (Array.isArray(newTarget)) player.outputChatBox(`!${COLOR.ERROR}* Múltiplos jogadores encontrados.`);
+    if(player.adminLevel < USER_MOD) player.outputChatBox(`!${COLOR.ERROR}* Você não tem permissão.`);
+    else if(!newTarget) player.outputChatBox(`!${COLOR.ERROR}* Jogador não encontrado.`);
+    else if(Array.isArray(newTarget)) player.outputChatBox(`!${COLOR.ERROR}* Múltiplos jogadores encontrados.`);
     else {
         let reason = '';
-        if (newTarget != player) {
+        if(newTarget != player) {
         mp.players.sendChatMessage(`!${COLOR.ADMIN_ACTION}* ${player.name} baniu ${newTarget.name} do servidor.`, 1, false); // global
-            if (args.length > 0) {
+            if(args.length > 0) {
                 reason = args.join(' ');
                 mp.players.sendChatMessage(`!${COLOR.ADMIN_ACTION}* Motivo: ${reason}.`, 1, false); // global
             }
@@ -42,3 +43,18 @@ const banCommand = (player: PlayerMp, fullText: string, target: string, ...args:
     }
 }
 mp.events.addCommand('ban', banCommand);
+
+const createCarCommmand = (player: PlayerMp, fullText: string, vehicle: string) => {
+    if(!fullText) return player.outputChatBox(`${COLOR.INFO}* /veh [vehicleid or vehiclename] `);
+
+    if(player.adminLevel < USER_ADMIN) player.outputChatBox(`!${COLOR.ERROR}* Você não tem permissão.`);
+    else {
+        const veh = mp.vehicles.new(mp.joaat(vehicle), player.position, {
+            numberPlate: "ADMIN",
+            color: [[2, 36, 255], [255, 255, 255]], // tribute for David Silva (SkyHawks Forever)
+            dimension: player.dimension
+        });
+        player.putIntoVehicle(veh, -1);
+    }
+}
+mp.events.addCommand('veh', createCarCommmand);
